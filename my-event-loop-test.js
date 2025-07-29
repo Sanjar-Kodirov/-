e577691ca63eb4f6c1d68c8d918ab5e25e2ddd7e
Sanjar Kodirov <kodirov.sanzhat@bk.ru>
2025-07-29 18:19:51 +0500

    Day 1 completed: Event Loop mastery - theory and practice

--- my-event-loop-test.js
+++ my-event-loop-test.js
@@ -2,14 +2,20 @@
 // Создайте код, который выведет числа от 1 до 5 в правильном порядке
 // используя setTimeout и Promise
 
-console.log('Мой тест Event Loop:');
+console.log("Мой тест Event Loop:");
 
 // TODO: Напишите код здесь
 // Цель: вывести 1, 2, 3, 4, 5 в правильном порядке
 // Используйте комбинацию:
 // - console.log (синхронный код)
-// - setTimeout (макрозадачи) 
+// - setTimeout (макрозадачи)
 // - Promise.resolve().then() (микрозадачи)
+
+console.log(1);
+Promise.resolve().then(() => console.log(2));
+Promise.resolve().then(() => console.log(3));
+setTimeout(() => console.log(3), 0);
+setTimeout(() => console.log(4), 0);
 
 // Пример структуры:
 // console.log(1);
